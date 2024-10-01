1f1af2b48bc4efe043dca34e3f02cf0ac7fe0de5
sadikaVER <sadika.verma@gmail.com>
2024-10-01 19:48:58 +0530

    update error in setupfile

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import setuptools
 
-with open("README.md","r", encoding="utf-8") as f:
-    long_description=f.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
-__version__="0.0.0"
 
-REPO_NAME="Text-Summarizer"
-AUTHOR_USER_NAME="sadikaver"
-SRC_REPO="textSummarizer"
-AUTHOR_EMAIL="sadika.verma@gmail.com"
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer"
+AUTHOR_USER_NAME = "sadikaVER"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "sadika.verma@gmail.com"
 
 
 
@@ -17,17 +18,13 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="Text Summarization using Transformers",
+    description="A small python package for NLP app",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/"+AUTHOR_USER_NAME+"/"+REPO_NAME,
-
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Source Code": "https://github.com/"+AUTHOR_USER_NAME+"/"+REPO_NAME,
-        "Bug Tracker": "https://github.com/"+AUTHOR_USER_NAME+"/"+REPO_NAME+"/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-
-    package_dirs={"": "src"},
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
-
 )
